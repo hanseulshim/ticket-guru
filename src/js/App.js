53e66bad37ef3363df5bd35c4d358f057e1f7fa2
hanseulshim <hanseulshim@gmail.com>
2018-02-24 14:38:53 -0500

    grab list of venues when the app loads

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,17 @@ import 'bulma/css/bulma.css'
 import Header from './components/Header';
 import TicketContainer from './components/TicketContainer';
 
+import config from './config/config';
+import httpRequest from './utils/httpRequest';
+
 class App extends Component {
+  componentDidMount() {
+    httpRequest.fetchJSON(config.venuesUrl, 'GET')
+    .then(result => {
+      console.log('result', result);
+    })
+  }
+
   render() {
     return (
       <div className="App">

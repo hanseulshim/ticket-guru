4633dcb912bc5616b09cf6c3b13b448c7ef6a8e4
hanseulshim <hanseulshim@gmail.com>
2018-02-24 16:48:06 -0500

    add styles for venuesContainer

---
+++ src/styles/venuesContainerStyle.js
@@ -0,0 +1,12 @@
+const styles = {
+  container: {
+    padding: '1rem 1.5rem',
+    boxSizing: 'border-box'
+  },
+  title: {
+    margin: '2rem 0',
+    fontWeight: '500'
+  }
+}
+
+export default styles;

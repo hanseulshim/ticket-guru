e7df7f11ab0583818910886787b405d71030b9c6
hanseulshim <hanseulshim@gmail.com>
2018-02-24 16:48:52 -0500

    add card styling

---
+++ src/styles/cardStyle.js
@@ -0,0 +1,33 @@
+const styles = {
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  card: {
+    width: '500px',
+    margin: '1rem',
+    padding: '1rem',
+    background: '#c9d8c5'
+  },
+  title: {
+    margin: '1rem 0 1.5rem 0',
+    color: '#3b3a36',
+    fontSize: '20px',
+    fontWeight: '600'
+  },
+  button: {
+    color: '#7d4627',
+    fontSize: '16px'
+  },
+  sectionTitle: {
+    margin: '0.5rem 0',
+    fontWeight: '600'
+  },
+  sectionContent: {
+    margin: '1rem 0',
+    color: '#3b3a36',
+    textDecoration: 'none'
+  }
+}
+
+export default styles;

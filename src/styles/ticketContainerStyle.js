50af54b4b299da66976596c5914dc0e8dc9da598
hanseulshim <hanseulshim@gmail.com>
2018-02-24 16:48:13 -0500

    add styles for ticketContainer

---
+++ src/styles/ticketContainerStyle.js
@@ -0,0 +1,10 @@
+const ticketContainerStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  flexDirection: 'column',
+  boxSizing: 'border-box',
+};
+
+module.exports = {
+  ticketContainerStyle
+};

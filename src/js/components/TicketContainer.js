578189d5ac7f7b6956c526b4cd2d5cf247961e82
hanseulshim <hanseulshim@gmail.com>
2018-02-24 18:04:32 -0500

    grab most recent venue and show

--- src/js/components/TicketContainer.js
+++ src/js/components/TicketContainer.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import VenuesContainer from './VenuesContainer'
+import VenuesContainer from './VenuesContainer';
+import ShowsContainer from './ShowsContainer';
+import config from '../config/config';
+import httpRequest from '../utils/httpRequest';
 import { ticketContainerStyle } from '../../styles/ticketContainerStyle';
 
 class TicketContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      selectedVenue: {}
+      selectedVenue: {},
+      selectedShow: {}
     }
   }
 
   selectVenue = (id) => {
     const tempVenues = this.props.venues.slice();
     const index = tempVenues.findIndex(venue => venue.id === id);
-    const selectedVenue = (index !== -1 && id !== this.state.selectedVenue.id) ? tempVenues[index] : {};
-    this.setState({ selectedVenue });
+    if (index !== -1 ) {
+      httpRequest.fetchJSON(`${config.venuesUrl}/${id}`, 'GET')
+      .then(response => {
+        const venue = JSON.parse(response);
+        this.setState({ 
+          selectedVenue: venue
+        });
+      })
+      .catch(err => {
+        console.log('Error in grabbing selected Venue', err);
+      })
+    } else {
+      this.setState({ selectedVenue: {} });
+    }
+  }
+
+  selectShow = (id) => {
+    const tempShows = this.state.selectedVenue.shows.slice();
+    const index = tempShows.findIndex(show => show.id === id);
+    if (index !== -1 ) {
+      httpRequest.fetchJSON(`${config.venuesUrl}/${this.state.selectedVenue.id}/shows/${id}`, 'GET')
+      .then(response => {
+        const show = JSON.parse(response);
+        this.setState({ 
+          selectedShow: show
+        });
+      })
+      .catch(err => {
+        console.log('Error in grabbing selected Show', err);
+      })
+    } else {
+      this.setState({ selectedShow: {} });
+    }
   }
 
   render() {
     const { venues } = this.props;
-    const { selectedVenue } = this.state;
+    const { selectedVenue, selectedShow } = this.state;
     return ( 
       <div style={ticketContainerStyle}>
         <VenuesContainer venues={venues} selectedVenueID={selectedVenue.id} selectVenue={this.selectVenue}/>
+        {
+          Object.keys(selectedVenue).length !== 0 ? 
+          <ShowsContainer shows={selectedVenue.shows} selectedShowID={selectedShow.id} selectShow={this.selectShow} /> : null
+        }
       </div>
     )
   }

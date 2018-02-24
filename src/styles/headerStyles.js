bf1609998adf25ebd31d233e71b4ec148d2f4cdf
hanseulshim <hanseulshim@gmail.com>
2018-02-24 16:48:43 -0500

    add style for header component

---
+++ src/styles/headerStyles.js
@@ -0,0 +1,22 @@
+const styles = {
+  container: {
+    width: '100%',
+    background: '#c0dfd9',
+    padding: '1rem 1.5rem',
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottom: '1px solid #3b3a36'
+  },
+  title: {
+    margin: '0.5rem 0'
+  },
+  subtitle: {
+    margin: '0',
+    fontWeight: 'lighter'
+  }
+}
+
+export default styles;

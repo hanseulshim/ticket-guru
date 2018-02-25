a9d0ca45bc8b8c3d552828e0a4af2a77adff40ed
hanseulshim <hanseulshim@gmail.com>
2018-02-24 19:47:37 -0500

    add url for customer info

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,7 +1,9 @@
 const url = 'http://ec2-54-167-181-205.compute-1.amazonaws.com:8080/ticket-guru/v1/api';
+const customerUrl = `${url}/customers`;
 const venuesUrl = `${url}/venues`;
 
 module.exports = {
   url,
+  customerUrl,
   venuesUrl
 }

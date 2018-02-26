3431109c918331399c757ac54cb54339139fbe63
hanseulshim <hanseulshim@gmail.com>
2018-02-25 23:36:49 -0500

    seatsContainer sends off make reservation request

--- src/js/components/SeatsContainer.js
+++ src/js/components/SeatsContainer.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
 import styles from '../../styles/sectionContainerStyle';
-
-import Select from 'react-select';
-import '../../styles/react-select.css';
+import cardStyles from '../../styles/cardStyle';
 
 class SeatsContainer extends Component {
 
   render() {
-    const { seatsAvailable } = this.props;
+    const { seatsAvailable, requestReservation, numSeats, updateNumSeats } = this.props;
 
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>
           Available Seats - {seatsAvailable}
         </h1>
+        <TextField
+          id="number"
+          label="Number of seats requested"
+          value={numSeats}
+          onChange={updateNumSeats}
+          type="number"
+          style={styles.textField}
+        />
+        <div>
+          <Button variant="raised" style={cardStyles.button} onClick={requestReservation}>
+            Make a Reservation
+          </Button>
+        </div>
       </div>
     )
   }

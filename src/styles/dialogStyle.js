3042ac9138b425a7581d3f17b968a5a890c69f99
hanseulshim <hanseulshim@gmail.com>
2018-02-25 23:35:52 -0500

    add dialog styles

---
+++ src/styles/dialogStyle.js
@@ -0,0 +1,14 @@
+const styles = {
+  container: {
+    width: '80%'
+  },
+  contentContainer: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  infoBox: {
+    marginRight: '5rem'
+  }
+}
+
+export default styles;
